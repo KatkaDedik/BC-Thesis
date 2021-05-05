499b43b222f253e69aa6677df0eb1c735d16d35d
Katarina Dedikova <xdediko1k@fi.muni.cz>
2021-05-05 18:31:45 +0200

    Fix pinch in zero gravity

--- Assets/Scripts/ZeroGravityMovement.cs
+++ Assets/Scripts/ZeroGravityMovement.cs
@@ -14,49 +14,39 @@ public class ZeroGravityMovement : MonoBehaviour
     public float RotationSpeed = 0.1f;
     public float Sensitivity = 1;
     public Transform RotationHelper = null;
+    public float pitch;
     
     private Vector3 direction;
     private VRCharacterController controller;
     private float speed = 0f;
     private float energy;
 
-    private LineRenderer line;
-    private Vector3[] positions;
-
     private void Start()
     {
         controller = GetComponent<VRCharacterController>();
         energy = MaxEnergy;
-        line = GetComponent<LineRenderer>();
-        positions = new Vector3[2];
     }
 
     private void Update()
     {
-        //Figure out movement orientation
-        Vector3 orientationEuler = new Vector3(controller.Head.localEulerAngles.x, controller.Head.localEulerAngles.y, 0.0f);
-        Quaternion orientation = Quaternion.Euler(orientationEuler);
-
-        var rotatedForward = controller.Head.localRotation * Vector3.forward;
-        rotatedForward.x = 0;
-        rotatedForward.Normalize();
-        //var cos = Vector3.Dot(rotatedForward, Vector3.forward);
-
-        var angle = Quaternion.Angle(controller.Head.localRotation, Quaternion.Euler(rotatedForward));
-
-        //Move RotationHelper
-        RotationHelper.position = controller.groundCheck.position;
-        var rotation = Mathf.Clamp(angle * RotationSpeed, -RotationSpeed, RotationSpeed);
-        RotationHelper.localPosition = new Vector3(RotationHelper.localPosition.x, RotationHelper.localPosition.y, RotationHelper.localPosition.z - rotation);
-
-        positions[0] = controller.Head.position;
-        positions[1] = RotationHelper.position;
-        line.SetPositions(positions);
 
         if (RocketMovePress.GetState(SteamVR_Input_Sources.Any) && energy > 0)
         {
-            
+            //Figure out movement orientation
+            Vector3 orientationEuler = new Vector3(controller.Head.localEulerAngles.x, controller.Head.localEulerAngles.y, 0.0f);
+            Quaternion orientation = Quaternion.Euler(orientationEuler);
 
+            //Move RotationHelper
+            RotationHelper.position = controller.groundCheck.position;
+            pitch = controller.Head.localEulerAngles.x;
+            if (pitch > 180)
+            {
+                pitch -= 360;
+            }
+            //Debug.Log($"pitch = {pitch}");
+            var rotation = Mathf.Clamp(pitch * RotationSpeed * Time.deltaTime, -RotationSpeed, RotationSpeed);
+            RotationHelper.localPosition = new Vector3(RotationHelper.localPosition.x, RotationHelper.localPosition.y, RotationHelper.localPosition.z - rotation);
+            
             var axis = Mathf.Clamp(RocketMoveValue.axis.y, 0, 1);
             speed += axis * Sensitivity;
             energy -= Time.deltaTime * RocketMoveValue.axis.y;
